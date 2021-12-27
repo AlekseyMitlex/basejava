319d14bc958105fc8047e99c0f22b401b04d50f3
AlekseyMitlex <aamtr@yandex.ru>
2021-12-27 19:19:27 +0700

    2 pravki. delau delete

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,58 +1,46 @@
 import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
+
     Resume[] storage = new Resume[10000];
     int size;
 
     void clear() {
-        Arrays.fill(storage, 0, size(), 0);
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     void save(Resume r) {
-        for (int i = 0; i < storage.length - 1; i++) {
-            if (storage[i] == null) {
-                storage[i] = r;
-                size++;
-            } else if (storage[i].uuid.equals(r.uuid)) {
-                System.out.println("Такое резюме уже есть");
-            }
-        }
+        int index = Math.max(size, 0);
+        storage[index] = r;
+        size++;
     }
 
     Resume get(String uuid) {
-
-//        for (int i = 0; i < storage.length; i++) {
-//            while (storage[i] != null) {
-//                if (storage[i].uuid.equals(uuid)) {
-//                    return storage[i];
-//                }
-//            }
-//        }
-//        return null;
-//    }
-        int i = 0;
-        while (storage[i] != null) {
-            if (storage[i].uuid.equals(uuid))
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
                 return storage[i];
-            i++;
+            }
         }
         return null;
     }
 
     void delete(String uuid) {
-
+        int index = 0;
+        System.arraycopy(storage, index + 1, storage, index, storage.length - index - 1);
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOf(storage, size());
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }

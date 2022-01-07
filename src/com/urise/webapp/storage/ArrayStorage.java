4147f3984bf6a22b11b48f29ba7a6d4fa8c408e2
AlekseyMitlex <aamtr@yandex.ru>
2022-01-07 11:54:02 +0700

    Introduce package + encapsulation

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -1,4 +1,5 @@
 package com.urise.webapp.storage;
+
 import com.urise.webapp.model.Resume;
 import java.util.Arrays;
 
@@ -6,41 +7,41 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
-    int size;
+    private Resume[] storage = new Resume[10000];
+    private int size;
 
-    void clear() {
+    public void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
-    void update(Resume r) {
+    public void update(Resume r) {
         System.out.println("ERROR: код ошибки");
     }
 
-    void save(Resume r) {
+    public void save(Resume r) {
         storage[size] = r;
         size++;
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(uuid)) {
+            if (storage[i].getUuid().equals(uuid)) {
                 return storage[i];
             }
         }
         return null;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
         int index = 0;
         for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(uuid)) {
+            if (storage[i].getUuid().equals(uuid)) {
                 index = i;
                 break;
             }
         }
-        storage[index] = storage [size - 1];
+        storage[index] = storage[size - 1];
         storage[size - 1] = null;
         size--;
     }
@@ -48,11 +49,11 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         return Arrays.copyOf(storage, size);
     }
 
-    int size() {
+    public int size() {
         return size;
     }
 }

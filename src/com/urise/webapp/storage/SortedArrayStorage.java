c3e9eb605038bd7b01cdc84c1335ea66ebb16eb3
AlekseyMitlex <aamtr@yandex.ru>
2022-01-20 08:15:38 +0700

    add to SortedArrayStorage Delete with check/without index

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -7,26 +7,6 @@ import java.util.Arrays;
 public class SortedArrayStorage extends AbstractArrayStorage{
 
     @Override
-    public void clear() {
-
-    }
-
-    @Override
-    public void update(Resume resume) {
-
-    }
-
-    @Override
-    public void delete(String uuid) {
-
-    }
-
-    @Override
-    public Resume[] getAll() {
-        return new Resume[0];
-    }
-
-    @Override
     protected void addNewResume(Resume resume) {
         int index = -(getIndex(resume.getUuid()) + 1);
         System.arraycopy(storage, index, storage, index + 1, size - index);
@@ -35,7 +15,7 @@ public class SortedArrayStorage extends AbstractArrayStorage{
 
     @Override
     protected void deleteResume(String uuid, int index) {
-
+        System.arraycopy(storage, index, storage, index + 1, size - index);
     }
 
     @Override

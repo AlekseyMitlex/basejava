ef7eb83c466bb8d0489ab82f2d5e37667246ac14
AlekseyMitlex <aamtr@yandex.ru>
2022-01-21 19:15:37 +0700

    fix SortedArrayStorage      deleteResume

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -4,7 +4,7 @@ import com.urise.webapp.model.Resume;
 
 import java.util.Arrays;
 
-public class SortedArrayStorage extends AbstractArrayStorage{
+public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     protected void addNewResume(Resume resume, int index) {
@@ -15,7 +15,7 @@ public class SortedArrayStorage extends AbstractArrayStorage{
 
     @Override
     protected void deleteResume(int index) {
-        System.arraycopy(storage, index, storage, index + 1, size - index);
+        System.arraycopy(storage, index, storage, index + 1, size - index - 1);
     }
 
     @Override

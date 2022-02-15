6fcff1f1f9b9931689532a842e32f442be542ba1
AlekseyMitlex <aamtr@yandex.ru>
2022-02-15 16:46:11 +0700

    add static block inicializacii

--- src/com/urise/webapp/MainCollections.java
+++ src/com/urise/webapp/MainCollections.java
@@ -2,24 +2,30 @@ package com.urise.webapp;
 
 import com.urise.webapp.model.Resume;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Objects;
+import java.util.*;
 
 public class MainCollections {
 
     private static final String UUID_1 = "uuid1";
-    private static final Resume RESUME_1 = new Resume(UUID_1);
+    private static final Resume RESUME_1;
 
     private static final String UUID_2 = "uuid2";
-    private static final Resume RESUME_2 = new Resume(UUID_2);
+    private static final Resume RESUME_2;
 
     private static final String UUID_3 = "uuid3";
-    private static final Resume RESUME_3 = new Resume(UUID_3);
+    private static final Resume RESUME_3;
 
     private static final String UUID_4 = "uuid4";
-    private static final Resume RESUME_4 = new Resume(UUID_4);
+    private static final Resume RESUME_4;
+
+    // Пример статического поля, для инициализации и обработки,
+    // может пригодиться. Обрабатывается в классах сразу после полей
+    static {
+        RESUME_1 = new Resume(UUID_1);
+        RESUME_2 = new Resume(UUID_2);
+        RESUME_3 = new Resume(UUID_3);
+        RESUME_4 = new Resume(UUID_4);
+    }
 
     public static void main(String[] args) {
 
@@ -45,5 +51,13 @@ public class MainCollections {
             }
         }
         System.out.println(collection.toString());
+
+        Map<String, Resume> map = new HashMap<>();
+        map.put(UUID_1, RESUME_1);
+        map.put(UUID_2, RESUME_2);
+        map.put(UUID_3, RESUME_3);
+
+        System.out.println("map = " + map.keySet());
     }
+
 }
